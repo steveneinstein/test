2d27c554e97a2839ea478e184063490ad88d2e9c
steveneistein <einsteindsouza008@gmail.com>
2022-08-07 19:33:50 +0530

    this is my 1st code push

--- main.py
+++ main.py
@@ -8,6 +8,18 @@ d = {
     "surname":"dsouza"
 
 }
+d2 = {
+    "name":"einstein",
+    "email":"einstein@gmail.com",
+    "surname":"dsouza"
+
+}
+d2 = {
+    "name":"einstein",
+    "email":"einstein@gmail.com",
+    "surname":"dsouza"
+
+}
 
 db1=client['mongotest']
 coll=db1['test']
